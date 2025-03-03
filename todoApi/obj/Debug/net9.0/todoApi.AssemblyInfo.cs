c4b96b3606a3abb5b50192e3f2ca426ccc4b9339
MissKluck <sigrid.b.kluck@gmail.com>
2025-03-03 13:31:52 +0100

    testing

--- todoApi/obj/Debug/net9.0/todoApi.AssemblyInfo.cs
+++ todoApi/obj/Debug/net9.0/todoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33348df29a8dd075cceec18810d74463730689c")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
